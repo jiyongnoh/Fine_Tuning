0a25ce57b6aa9a17cd1b472aef22c942d4dc6e28
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2023-12-12 14:24:20 +0900

    comment: 주석 추가

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const openai = new OpenAI({
 //   purpose: "fine-tune",
 // });
 
-// Fine Tuning 시작
+// Fine Tuning 시작 - 기존 Fine Tuning 완료 모델을 이어서 학습 가능
 const fineTune = await openai.fineTuning.jobs.create({
   training_file: "file-Trp8wJ4G0dtNBeAvXoxG7hWy", // 훈련 파일. 공홈 File 태그의 ID
   model: "ft:gpt-3.5-turbo-1106:personal::8UQytKPc", // 훈련 모델. Fine Tuning 태그의 모델 ID
